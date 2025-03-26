c24201849f1f31598b5190ade4d2dbae14e88db1
wstinkens <wouterstinkens@gmail.com>
2025-03-26 15:53:50 +0100

    Fixed path not found error

--- utils/file.py
+++ utils/file.py
@@ -1,8 +1,10 @@
+import os
+
 def read_queries_file():
     """
     Reads the queries.txt file and returns its content as text.
     """
-    file_path = "../files/queries.txt"
+    file_path = os.path.join(os.path.dirname(__file__), "../files/queries.txt")
     with open(file_path, 'r') as file:
         content = file.read()
     return content
